99530164d6993e21597fddeafa9af9223d5b10d1
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-06 13:46:48 +0530

    added more components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
+
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
   }
   
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-    </div>
-  );
 }
 
 export default App;
